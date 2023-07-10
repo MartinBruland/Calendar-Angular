e1024762e9a93d6aa8a9304c7a71edb5c3c031a2
Martin Bruland <martin.bruland@tietoevry.com>
2023-07-10 14:10:12 +0200

    added ios dev folder

---
+++ calendar-ios/calendar-ios/calendar_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendar_iosApp.swift
+//  calendar-ios
+//
+//  Created by Martin Bruland on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct calendar_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
